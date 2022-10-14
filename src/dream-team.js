188eeb5e5a05a67aaabfa57d080597b6d51eb98f
Victoria Chernikova <victoria.chernikova@outlook.com>
2022-10-14 12:32:19 +0300

    feat: add task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(members) {
+  let result = '';
+  if(!Array.isArray(members) || members.length == 0) {
+    return false;
+  }
+  members.forEach(member => {
+    if(typeof(member) == 'string' && member.length > 0) {
+      return result += member.trim()[0].toUpperCase();
+    } 
+  });
+
+  return result.split('').sort().join("");
+};
 
 module.exports = {
   createDreamTeam
